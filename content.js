8bf5cd23cdabaa7cba5add572b381afa0acdccb8
Justin Hu <justinhu502@gmail.com>
2019-09-28 10:36:42 -0400

    Init commit

---
+++ content.js
@@ -0,0 +1,9 @@
+alert("Hello from your Chrome extension!")
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if( request.message === "clicked_browser_action" ) {
+       console.log("content.js running")
+      }
+    }
+  );
